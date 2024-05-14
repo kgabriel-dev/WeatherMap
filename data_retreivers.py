ac31cfb39a8070a9f2eaabd892351d17392a2dcd
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2024-05-14 15:02:06 +0200

    add timezone support for BrightSky

--- data_retreivers.py
+++ data_retreivers.py
@@ -8,18 +8,18 @@ import pytz
 class BrightSky:
 
     def __init__(self):
-        self.api_url = "https://api.brightsky.dev/weather?date={date}&last_date={last_date}&lat={lat}&lon={lon}"
+        self.api_url = "https://api.brightsky.dev/weather?date={date}&last_date={last_date}&lat={lat}&lon={lon}&tz={timezone}"
         self.min_value = 0
         self.max_value = 100
         self.request_delay = 0.5
 
     
-    def get_weather(self, start_date_iso, last_date_iso, lat, lon):
+    def get_weather(self, start_date_iso, last_date_iso, lat, lon, timezone):
         # reset starting time to given hour
         start_date = datetime.fromisoformat(start_date_iso)
         start_date = start_date.replace(minute=0, second=0, microsecond=0)
 
-        url = self.api_url.format(date=start_date.isoformat(), last_date=last_date_iso, lat=lat, lon=lon)
+        url = self.api_url.format(date=start_date.isoformat(), last_date=last_date_iso, lat=lat, lon=lon, timezone=timezone)
         
         request = requests.get(url)
 
