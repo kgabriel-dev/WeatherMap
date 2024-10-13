4b3b0c855768b021328c97df02e5773f5b9f9083
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2024-10-13 12:45:26 +0200

    add location markers to map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,8 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
+import { LocationService } from '../../services/location/location.service';
+import { SettingsService } from '../../services/settings/settings.service';
+import { BehaviorSubject, combineLatestWith, map } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -10,8 +13,49 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   private map: L.Map | undefined;
+  private isMapReady$ = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  readonly markerIcon = L.icon({
+    iconUrl: 'assets/maps-pin-black-icon.png',
+    iconSize: [ 25, 25 ],
+    iconAnchor: [ 13, 25 ]
+  })
+
+  readonly markerIconSelected = L.icon({
+    iconUrl: 'assets/maps-pin-red-icon.png',
+    iconSize: [ 25, 25 ],
+    iconAnchor: [ 13, 25 ]
+  })
+
+  markers: L.Marker[] = [];
+
+  constructor(
+    private locationsService: LocationService,
+    private settingsService: SettingsService
+  ) {
+    const combinedReadiness$ = this.locationsService.isServiceReady().pipe(
+      combineLatestWith(this.settingsService.isServiceReady(), this.isMapReady$),
+      map(([isLocationReady, isSettingsReady, isMapReady]) => isLocationReady && isSettingsReady && isMapReady)
+    );
+
+    // --> subscribe to the observable
+    combinedReadiness$.subscribe((ready: boolean) => {
+      if(!ready || !this.map)
+        return;
+
+      const locations = this.locationsService.getLocations();
+      for(let i = 0; i < locations.length; i++) {
+        const location = locations[i];
+        const icon = i === this.settingsService.getSettings().defaultLocationIndex ? this.markerIconSelected : this.markerIcon;
+
+        this.markers.push(
+          L.marker([ location.coordinates.latitude, location.coordinates.longitude ], { icon })
+            .addTo(this.map)
+            .bindPopup(location.name)
+        )
+      }
+    })
+  }
 
   ngAfterViewInit() {
     this.initMap();
@@ -30,6 +74,8 @@ export class MapComponent implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
+
+    this.isMapReady$.next(true);
   }
 
 }
