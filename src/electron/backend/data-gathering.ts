107586bb0ca4adeffbd5f1ae7e685e9a4476a67e
GabiKleebi02 <gabrielkleebaum@yahoo.de>
2024-12-01 21:07:20 +0100

    fixed bug with grid locations in data gathering

--- src/electron/backend/data-gathering.ts
+++ src/electron/backend/data-gathering.ts
@@ -18,8 +18,8 @@ export class OpenMeteoDataGatherer implements DataGatherer {
       const stepSize = regionSizeInKm / region.region.resolution;
       const latStepSize = stepSize / 110.574; // 1° latitude is 110.574 km
       const lonStepSize = stepSize / (111.320 * Math.cos(region.coordinates.latitude * Math.PI / 180)); // 1° longitude is 111.320 km at the equator
-      const locationOffset = -Math.floor(region.region.resolution/2) + (region.region.resolution % 2 === 0 ? 0.5 : 1);
-
+      const locationOffset = Math.floor(-region.region.resolution/2) + (region.region.resolution % 2 === 0 ? 0.5 : 1); // how many steps to the left and top from the center
+      console.log(locationOffset);
       const requests: { lat: number, lon: number, api: string, hours: number, tz: string }[] = [];
 
       if(cancelRequested) {
@@ -30,8 +30,8 @@ export class OpenMeteoDataGatherer implements DataGatherer {
 
       for(let latIndex = 0; latIndex < region.region.resolution; latIndex++) {
         for(let lonIndex = 0; lonIndex < region.region.resolution; lonIndex++) {
-          const lat = region.coordinates.latitude + locationOffset + latIndex * latStepSize;
-          const lon = region.coordinates.longitude + locationOffset + lonIndex * lonStepSize;
+          const lat = region.coordinates.latitude + (locationOffset + latIndex) * latStepSize;
+          const lon = region.coordinates.longitude + (locationOffset + lonIndex) * lonStepSize;
 
           requests.push({ lat, lon, api: condition.api, hours: forecast_hours + 1, tz: region.timezoneCode });
         }
@@ -68,6 +68,8 @@ export class OpenMeteoDataGatherer implements DataGatherer {
         .replace('{category}', data.api)
         .replace('{hours}', data.hours.toString())
         .replace('{timezone}', data.tz);
+
+      console.log(url);
 
       try {
         const response = await fetch(url);
